e6304da971555a6ba7882525b920b2415198ea60
José Niño <33065915+joseomarnm@users.noreply.github.com>
2022-01-25 14:50:37 -0300

    Add multiply function

--- GitPlayGround/Functions.cs
+++ GitPlayGround/Functions.cs
@@ -15,5 +15,9 @@ namespace GitPlayGround
         public static int Substract(int a, int b) {
             return a - b;
         }
+
+        public static int Multiply(int a, int b) {
+            return a * b;
+        }
     }
 }
